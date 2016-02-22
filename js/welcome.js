63980840a909b730df454e1d82ae057df155d2c9
Kristel Teng <kristelteng@gmail.com>
2016-02-22 14:11:55 -0500

    add javascript for typing to the intro section

---
+++ js/welcome.js
@@ -0,0 +1,12 @@
+
+$(function() {
+    $(".typing-element").typed({
+        strings: ["Kristel", "Developer", "UX/UI Designer", "Mother"], 
+        typeSpeed: 100,
+        contentType: 'html',
+        loop: true,
+        loopCount: 10,
+        showCursor: true,
+        cursorChar: "|",
+    });
+});
