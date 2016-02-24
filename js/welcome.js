bbb427c0cd5668834894ec69f52a2ede6d694147
Kristel Teng <kristelteng@gmail.com>
2016-02-24 15:33:19 -0500

    add description about me

--- js/welcome.js
+++ js/welcome.js
@@ -1,7 +1,7 @@
 
 $(function() {
     $(".typing-element").typed({
-        strings: ["Kristel", "Developer", "UX/UI Designer", "Mother"], 
+        strings: ["Kristel", "a Developer", "a UX/UI Designer", "ambitious", "friendly", "passionate", "talented"], 
         typeSpeed: 100,
         contentType: 'html',
         loop: true,
