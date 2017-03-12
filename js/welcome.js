f8fb550a0efd2167ea74897fdffb3b7232303fdb
Kristel Teng <kristelteng@gmail.com>
2017-03-11 22:48:19 -0800

    add typing for desktop, but when on mobile typing is deacivated

--- js/welcome.js
+++ js/welcome.js
@@ -1,12 +1,38 @@
+// $(function() {
+//   $(".typing-element").typed({
+//     strings: ["Kristel", "a Developer", "a UX/UI Designer", "ambitious", "friendly", "Kristel"],
+//     typeSpeed: 100,
+//     contentType: 'html',
+//     loop: true,
+//     loopCount: 1,
+//     showCursor: true,
+//     cursorChar: "|",
+//   });
+// });
 
-$(function() {
+function isMobile() {
+ return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+if(!isMobile()) {
+  $(function() {
     $(".typing-element").typed({
-        strings: ["Kristel", "a Developer", "a UX/UI Designer", "ambitious", "friendly", "Kristel"],
-        typeSpeed: 100,
-        contentType: 'html',
-        loop: true,
-        loopCount: 1,
-        showCursor: true,
-        cursorChar: "|",
+      strings: ["Kristel", "a Developer", "a UX/UI Designer", "ambitious", "friendly", "Kristel"],
+      typeSpeed: 100,
+      contentType: 'html',
+      loop: true,
+      loopCount: 1,
+      showCursor: true,
+      cursorChar: "|",
     });
-});
+  });
+}
+
+
+
+// $(document).ready(function()
+// {
+//   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(navigator.userAgent) == false) {
+//     new simpleIntro();
+//   }
+// });
