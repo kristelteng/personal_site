1ab9cb8de3b6745d71dc3c3280c0f4b99ed7ec55
kristel <kristelteng@gmail.com>
2021-10-10 08:02:15 -0700

    clean up, remove unused code

--- js/main.js
+++ js/main.js
@@ -1,119 +1,24 @@
-jQuery(function($) {'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+	let mainNav = document.querySelectorAll('.nav-link'),
+		subNav = document.querySelectorAll('.sub-link'),
+		codePg = window.location.pathname.includes('/work/code/'),
+		productPg = window.location.pathname.includes('/work/product/'),
+		designPg = window.location.pathname.includes('/work/design/');
+	
+	if (codePg || productPg || designPg) {
 
-	//Responsive Nav
-	$('li.dropdown').find('.fa-angle-down').each(function(){
-		$(this).on('click', function(){
-			if( $(window).width() < 768 ) {
-				$(this).parent().next().slideToggle();
+		for (let i = 0; i < mainNav.length; i++) {
+			if (mainNav[i].innerHTML === 'work') {
+				mainNav[i].classList.add('current');
 			}
-			return false;
-		});
-	});
 
-	//Fit Vids
-	if( $('#video-container').length ) {
-		$("#video-container").fitVids();
-	}
-
-	//Initiat WOW JS
-	new WOW().init();
-
-	// portfolio filter
-	$(window).load(function(){
-
-		$('.main-slider').addClass('animate-in');
-		$('.preloader').remove();
-		//End Preloader
-
-		if( $('.masonery_area').length ) {
-			$('.masonery_area').masonry();//Masonry
-		}
-
-		var $portfolio_selectors = $('.portfolio-filter >li>a');
-		
-		if($portfolio_selectors.length) {
-			
-			var $portfolio = $('.portfolio-items');
-			$portfolio.isotope({
-				itemSelector : '.portfolio-item',
-				layoutMode : 'fitRows'
-			});
-			
-			$portfolio_selectors.on('click', function(){
-				$portfolio_selectors.removeClass('active');
-				$(this).addClass('active');
-				var selector = $(this).attr('data-filter');
-				$portfolio.isotope({ filter: selector });
-				return false;
-			});
-		}
-
-	});
-
-
-	$('.timer').each(count);
-	function count(options) {
-		var $this = $(this);
-		options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-		$this.countTo(options);
-	}
-		
-	// Search
-	$('.fa-search').on('click', function() {
-		$('.field-toggle').fadeToggle(200);
-	});
-
-	// Contact form
-	var form = $('#main-contact-form');
-	form.submit(function(event){
-		event.preventDefault();
-		var form_status = $('<div class="form_status"></div>');
-		$.ajax({
-			url: $(this).attr('action'),
-			beforeSend: function(){
-				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+			if (codePg && subNav[i].innerHTML === 'coding') {
+				subNav[i].classList.add('current');
+			} else if (productPg && subNav[i].innerHTML === 'product &amp; packaging design') {
+				subNav[i].classList.add('current');
+			} else if (designPg && subNav[i].innerHTML === 'branding') {
+				subNav[i].classList.add('current');
 			}
-		}).done(function(data){
-			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
-		});
-	});
-
-	// Progress Bar
-	$.each($('div.progress-bar'),function(){
-		$(this).css('width', $(this).attr('data-transition')+'%');
-	});
-
-	if( $('#gmap').length ) {
-		var map;
-
-		map = new GMaps({
-			el: '#gmap',
-			lat: 43.04446,
-			lng: -76.130791,
-			scrollwheel:false,
-			zoom: 16,
-			zoomControl : false,
-			panControl : false,
-			streetViewControl : false,
-			mapTypeControl: false,
-			overviewMapControl: false,
-			clickable: false
-		});
-
-		map.addMarker({
-			lat: 43.04446,
-			lng: -76.130791,
-			animation: google.maps.Animation.DROP,
-			verticalAlign: 'bottom',
-			horizontalAlign: 'center',
-			backgroundColor: '#3e8bff',
-		});
+		}
 	}
-
-	//set submenu navigation
-	$('.sub-nav > .nav-item').on('click', function() {
-		$('.nav-item').addClass('active');
-	});
-
-	$('.nav-item').removeClass('active');
-});
+}, false);
